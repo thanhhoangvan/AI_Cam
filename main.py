cf0a64d893cdf9effe34f26184f43b797cc9ac36
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-04-23 03:31:15 +0700

    updates

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
++============================================================+
+- Tác Giả: Hoàng Thành
+- Viện Toán Ứng dụng và Tin học(SAMI - HUST)
+- Email: thanh.hoangvan051199@gmail.com
+- Github: https://github.com/thanhhoangvan
++============================================================+
+"""
+
+import os
+import time
+
+import cv2
+import numpy as np
+import multiprocessing as mp
+from multiprocessing import Process, Array, Value, Lock
+
+
+class AICamera:
+    def __init__(self) -> None:
+        pass
+
+
+
--- utils/README.txt
+++ utils/README.txt
@@ -1,8 +1,6 @@
 Thư mục chứa các module chức năng mở rộng của chương trình
-- Cảnh báo điểm mù của hệ thống nhiều camera
-- PTZ control
-- Nhận diện đối tượng
+- Điều khiển PTZ
+- Nhận diện người
 - Cảnh báo cháy nổ
-- Cảnh báo xâm nhập trái phép
-- Cảnh báo có vũ khí
+- Ghi hình
 - updating
