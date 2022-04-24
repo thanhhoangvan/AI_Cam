04247770ec9745d0ca2867ccb12ccfc97131351f
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-04-24 17:15:33 +0700

    update FaceID module

--- utils/FaceID.py
+++ utils/FaceID.py
@@ -9,8 +9,38 @@
 
 import os
 import time
-import multiprocessing as mp
+import face_recognition
+import matplotlib.pyplot as plt
 
-import cv2
-import tensorflow as tf
 
+class FaceID:
+    def __init__(self):
+        self.ListFaceName = []
+        self.ListEncodeFace = []
+
+    def getFaceLocation(self, image):
+        return face_recognition.face_locations(image)[0]
+    
+    def getFaceEncode(self, image):
+        return face_recognition.face_encodings(image)[0]
+    
+    def addNewFace(self, image, name="Unknow"):
+        self.ListFaceName.append(name)
+        self.ListEncodeFace.append(self.getFaceEncode(image))
+        return
+    
+    def recognition(self, image):
+        FaceEncoded = self.getFaceEncode(image)
+        for FaceID in range(len(self.ListEncodeFace)):
+            result = face_recognition.compare_faces([self.ListEncodeFace[FaceID]], FaceEncoded)
+            if result:
+                return self.ListFaceName[FaceID]
+    
+    def show(self, image):
+        pass
+    
+    def deleteFace(self, image=None, name="Unknow"):
+        pass
+
+if __name__=='__main__':
+    pass
