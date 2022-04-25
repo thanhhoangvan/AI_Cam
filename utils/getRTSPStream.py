87ab4088109f2dc1760cf994c9424433f10d7bd7
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-04-25 15:35:19 +0700

    update module, still have bug when join process

--- utils/getRTSPStream.py
+++ utils/getRTSPStream.py
@@ -15,8 +15,7 @@ os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = 'rtsp_transport;udp'
 import json
 import time
 from threading import Thread
-from multiprocessing import Process, Array, Value, Lock
-from turtle import rt
+from multiprocessing import Process, Queue, Value, Lock
 
 import cv2
 import numpy as np
@@ -26,17 +25,18 @@ def get_RTSP_URL(username, password, ip, port):
     return ('rtsp://{}:{}@{}:{}/onvif1'.format(username, password, ip, port), # Full HD rtsp url
             'rtsp://{}:{}@{}:{}/onvif2'.format(username, password, ip, port)) #      SD rtsp url
 
+
 class RTSPStream:
     def __init__(self, ip="192.168.53.109", port=554,
                  username='admin', password='admin',
-                 HD_Frame=None, SD_Frame=None):
+                 queue_hd=None, queue_sd=None):
 
         self.IP          = ip
         self.PORT        = port
         self.user        = username
         self.passwd      = password
-        self.mp_HD_Frame = HD_Frame
-        self.mp_SD_Frame = SD_Frame
+        self.queue_hd_frame = queue_hd
+        self.queue_sd_frame = queue_sd
 
         (self.RTSP_URL_HD, self.RTSP_URL_SD) = get_RTSP_URL(self.user, self.passwd, self.IP, self.PORT)
 
@@ -62,56 +62,65 @@ class RTSPStream:
         while mp_running.value == 1:
             ret, frame = cap.read()
             if ret:
-                mp_frame.acquire()
-                mp_frame[:] = frame.flatten()
-                # try:
-                #     # mp_frame.release()
-                #     print('captured a frame from', URL)
-                # except ValueError:
-                #     print('Error when assign frame', URL)
+                if not mp_frame.full():
+                    mp_frame.put(frame)
             else:
                 print('Can not capture from', URL)
 
-            time.sleep(0.001)
-        
         print("Stop capture from", URL)
 
     def update(self):
         if self.HD_CaptureThread is None:
             self.HD_CaptureThread = Thread(target=self.capture, args=(self.RTSP_URL_HD,
-                                                                      self.mp_HD_Frame,
+                                                                      self.queue_hd_frame,
                                                                       self.mp_running))
         if self.SD_CaptureThread is None:
             self.SD_CaptureThread = Thread(target=self.capture, args=(self.RTSP_URL_SD,
-                                                                      self.mp_SD_Frame,
+                                                                      self.queue_sd_frame,
                                                                       self.mp_running))
         self.HD_CaptureThread.start()
         self.SD_CaptureThread.start()
         
-        while self.mp_running.value == 1:
-            pass
-            
-        self.HD_CaptureThread.join()
-        self.SD_CaptureThread.join()
+        while True:
+            if self.mp_running.value == 0:
+                time.sleep(0.005)
+                self.HD_CaptureThread.join()
+                self.SD_CaptureThread.join()
+                break
+        print('Joined all threads')
 
     def stop(self):
         if self.mp_running.value == 1:
             self.mp_running.value = 0
-        time.sleep(0.5)
-        self.processCapture.join()
+        time.sleep(0.1)
+
+        self.HD_CaptureThread = None
+        self.SD_CaptureThread = None
+
+        # Clean queue
+        print('Cleanning queue....')
+        while True:
+            _ = self.queue_hd_frame.get()
+            _ = self.queue_sd_frame.get()
+
+            if  self.queue_hd_frame.empty():
+                if  self.queue_sd_frame.empty():
+                    break        
+
+        print("HD queue is cleaned:", self.queue_hd_frame.empty())
+        print("SD queue is cleaned:", self.queue_sd_frame.empty())
+        
+        print('Stopping Process Capture... ')
+        if self.processCapture is not None:
+            self.processCapture.join()
 
 if __name__=='__main__':
-    HD_RESOLUTION = (1080, 1920, 3)
-    SD_RESOLUTION = (360, 640, 3)
 
-    mp_FRAME_HD = Array("I", int(np.prod(HD_RESOLUTION)), lock=Lock())
-    mp_FRAME_SD = Array("I", int(np.prod(SD_RESOLUTION)), lock=Lock())
-
-    hd_nparray = np.frombuffer(mp_FRAME_HD.get_obj(), dtype="I").reshape(HD_RESOLUTION)
-    sd_nparray = np.frombuffer(mp_FRAME_SD.get_obj(), dtype="I").reshape(SD_RESOLUTION)
+    queue_frame_hd = Queue(maxsize=100)
+    queue_frame_sd = Queue(maxsize=100)
 
     json_config = {}
-    with open('./config.json', 'r') as json_file:
+    with open('../config.json', 'r') as json_file:
         json_config = json.load(json_file)
 
     IP = json_config['rtsp']['ip']
@@ -121,27 +130,21 @@ if __name__=='__main__':
 
     rtsp_stream = RTSPStream(ip=IP, port=PORT,
                              username=USERNAME, password=PASSWORD,
-                             HD_Frame=mp_FRAME_HD, SD_Frame=mp_FRAME_SD)
+                             queue_hd=queue_frame_hd, queue_sd=queue_frame_sd)
     rtsp_stream.start()
     print('Start Successful!')
     
     start_time = time.time()
     while (time.time() - start_time) <= 30:
-        hd_frame = sd_nparray.astype("uint8").copy()
-        sd_frame = sd_nparray.astype("uint8").copy()
         
-        cv2.imshow('RTSP Streamming Result', hd_frame)
-        # cv2.imshow('RTSP Streamming Result', sd_frame)
+        hd_frame = queue_frame_hd.get()
+        sd_frame = queue_frame_sd.get()
         
-        try:
-            mp_FRAME_HD.release()
-            mp_FRAME_SD.release()
-        except ValueError:
-            pass
+        # cv2.imshow('RTSP Streamming Result', hd_frame)
+        cv2.imshow('RTSP Streamming Result', sd_frame)
 
-            if cv2.waitKey(1) == 27:
-                break
-        time.sleep(0.001)
+        if cv2.waitKey(1) == 27:
+            break
     
     cv2.destroyAllWindows()
     rtsp_stream.stop()    
